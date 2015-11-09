e60e2654d6a94b1c0f3b882caa4b4f189cbf98b3
Xiao Yao <xiaoyao@Xiaos-MacBook-Pro.local>
2015-11-09 16:21:04 -0500

    Initial Commit

---
+++ Donut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Donut
+//
+//  Created by Xiao Yao on 11/9/15.
+//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Donut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Donut
+//
+//  Created by Xiao Yao on 11/9/15.
+//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
